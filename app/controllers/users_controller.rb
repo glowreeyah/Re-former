ab4dc938f9f0f982e480209037f28a85f303d178
Glory David <glodave99@gmail.com>
2020-08-07 11:56:43 +0100

    add form_for and edit method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render edit_user_path(@user)
+    end
+  end
+
   private
 
   def user_params
